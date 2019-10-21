20d3883204da228cca68b74f3a572752e69430b4
hritikkothari <53790479+hrithikkothari1234@users.noreply.github.com>
2019-10-22 02:05:29 +0530

    Delete Logout.php

--- My Blog Website/Logout.php
+++
@@ -1,7 +0,0 @@
-<?php require_once("include/sessions.php"); ?>
-<?php require_once("include/Functions.php"); ?>
-<?php
-$_SESSION["User_Id"]=null;
-session_destroy();
-Redirect_to("Login.php");
- ?>
